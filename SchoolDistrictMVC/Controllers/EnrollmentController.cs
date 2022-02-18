2611df3c32d076e685e62ad054c23750350ba50e
Landon Shay <landonshay@gmail.com>
2022-02-18 05:38:31 -0500

    Added dropdown menu to Classes and fixed overflow problem

--- SchoolDistrictMVC/Controllers/EnrollmentController.cs
+++ SchoolDistrictMVC/Controllers/EnrollmentController.cs
@@ -1,159 +1,159 @@
-﻿using SchoolDistrictMVC.Data;
-using SchoolDistrictMVC.Models.Enrollment;
-using SchoolDistrictMVC.Services;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿//using SchoolDistrictMVC.Data;
+//using SchoolDistrictMVC.Models.Enrollment;
+//using SchoolDistrictMVC.Services;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Web;
+//using System.Web.Mvc;
 
-namespace SchoolDistrictMVC.Controllers
-{
-    public class EnrollmentController : Controller
-    {
-        public ActionResult Index()
-        {
-            var service = CreateEnrollmentService();
-            var model = service.GetEnrollmentList();
-            return View(model);
-        }
+//namespace SchoolDistrictMVC.Controllers
+//{
+//    public class EnrollmentController : Controller
+//    {
+//        public ActionResult Index()
+//        {
+//            var service = CreateEnrollmentService();
+//            var model = service.GetEnrollmentList();
+//            return View(model);
+//        }
 
-        public ActionResult Create()
-        {
-            List<Student> Students = new StudentService().GetStudents().ToList();
+//        public ActionResult Create()
+//        {
+//            List<Student> Students = new StudentService().GetStudents().ToList();
 
-            var query = from s in Students
-                        select new SelectListItem()
-                        {
-                            Value = s.Id.ToString(),
-                            Text = s.FullName,
-                        };
+//            var query = from s in Students
+//                        select new SelectListItem()
+//                        {
+//                            Value = s.Id.ToString(),
+//                            Text = s.FullName,
+//                        };
 
-            List<Class> Classes = new ClassService().GetClasses().ToList();
+//            List<Class> Classes = new ClassService().GetClasses().ToList();
 
-            var query2 = from s in Classes
-                        select new SelectListItem()
-                        {
-                            Value = s.Id.ToString(),
-                            Text = s.Name,
-                        };
+//            var query2 = from s in Classes
+//                        select new SelectListItem()
+//                        {
+//                            Value = s.Id.ToString(),
+//                            Text = s.Name,
+//                        };
 
-            ViewBag.StudentId = query.ToList();
-            ViewBag.ClassId = query2.ToList();
+//            ViewBag.StudentId = query.ToList();
+//            ViewBag.ClassId = query2.ToList();
 
-            return View();
-        }
+//            return View();
+//        }
 
-        /*[HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(EnrollmentCreate student)
-        {
-            if (!ModelState.IsValid) return View(student);
+//        /*[HttpPost]
+//        [ValidateAntiForgeryToken]
+//        public ActionResult Create(EnrollmentCreate student)
+//        {
+//            if (!ModelState.IsValid) return View(student);
 
-            var service = CreateEnrollmentService();
+//            var service = CreateEnrollmentService();
 
-            if (service.CreateEnrollment(student))
-            {
-                TempData["SaveResult"] = $"Assignment successful.";
-                return RedirectToAction("Index");
-            };
+//            if (service.CreateEnrollment(student))
+//            {
+//                TempData["SaveResult"] = $"Assignment successful.";
+//                return RedirectToAction("Index");
+//            };
 
-            ModelState.AddModelError("", $"Assignment unsuccessful");
-            return View(student);
-        }*/
+//            ModelState.AddModelError("", $"Assignment unsuccessful");
+//            return View(student);
+//        }*/
 
-        public ActionResult Details(int id)
-        {
-            var svc = CreateEnrollmentService();
-            var model = svc.GetEnrollmentById(id);
+//        public ActionResult Details(int id)
+//        {
+//            var svc = CreateEnrollmentService();
+//            var model = svc.GetEnrollmentById(id);
 
-            return View(model);
-        }
+//            return View(model);
+//        }
 
-        public ActionResult Edit(int id)
-        {
-            var service = CreateEnrollmentService();
-            var detail = service.GetEnrollmentById(id);
-            var model = new EnrollmentEdit
-            {
-                Id = detail.Id,
-                StudentId = detail.StudentId,
-                ClassId = detail.ClassId,
-            };
+//        public ActionResult Edit(int id)
+//        {
+//            var service = CreateEnrollmentService();
+//            var detail = service.GetEnrollmentById(id);
+//            var model = new EnrollmentEdit
+//            {
+//                Id = detail.Id,
+//                StudentId = detail.StudentId,
+//                ClassId = detail.ClassId,
+//            };
 
-            List<Student> Students = new StudentService().GetStudents().ToList();
+//            List<Student> Students = new StudentService().GetStudents().ToList();
 
-            var query = from s in Students
-                        select new SelectListItem()
-                        {
-                            Value = s.Id.ToString(),
-                            Text = s.FullName,
-                        };
+//            var query = from s in Students
+//                        select new SelectListItem()
+//                        {
+//                            Value = s.Id.ToString(),
+//                            Text = s.FullName,
+//                        };
 
-            List<Class> Classes = new ClassService().GetClasses().ToList();
+//            List<Class> Classes = new ClassService().GetClasses().ToList();
 
-            var query2 = from s in Classes
-                         select new SelectListItem()
-                         {
-                             Value = s.Id.ToString(),
-                             Text = s.Name,
-                         };
+//            var query2 = from s in Classes
+//                         select new SelectListItem()
+//                         {
+//                             Value = s.Id.ToString(),
+//                             Text = s.Name,
+//                         };
 
-            ViewBag.StudentId = query.ToList();
-            ViewBag.ClassId = query2.ToList();
+//            ViewBag.StudentId = query.ToList();
+//            ViewBag.ClassId = query2.ToList();
 
-            return View(model);
-        }
+//            return View(model);
+//        }
 
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, EnrollmentEdit student)
-        {
-            if (!ModelState.IsValid) return View(student);
+//        [HttpPost]
+//        [ValidateAntiForgeryToken]
+//        public ActionResult Edit(int id, EnrollmentEdit student)
+//        {
+//            if (!ModelState.IsValid) return View(student);
 
-            if (student.Id != id)
-            {
-                ModelState.AddModelError("", "Id Mismatch");
-                return View(student);
-            }
+//            if (student.Id != id)
+//            {
+//                ModelState.AddModelError("", "Id Mismatch");
+//                return View(student);
+//            }
 
-            var service = CreateEnrollmentService();
+//            var service = CreateEnrollmentService();
 
-            if (service.UpdateEnrollment(student))
-            {
-                TempData["SaveResult"] = $"Enrollment updated.";
-                return RedirectToAction("Index");
-            }
-            ModelState.AddModelError("", $"Update failed.");
-            return View();
-        }
+//            if (service.UpdateEnrollment(student))
+//            {
+//                TempData["SaveResult"] = $"Enrollment updated.";
+//                return RedirectToAction("Index");
+//            }
+//            ModelState.AddModelError("", $"Update failed.");
+//            return View();
+//        }
 
-        public ActionResult Delete(int id)
-        {
-            var svc = CreateEnrollmentService();
-            var model = svc.GetEnrollmentById(id);
+//        public ActionResult Delete(int id)
+//        {
+//            var svc = CreateEnrollmentService();
+//            var model = svc.GetEnrollmentById(id);
 
-            return View(model);
-        }
+//            return View(model);
+//        }
 
-        [HttpPost]
-        [ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteEnrollment(int id)
-        {
-            var service = CreateEnrollmentService();
+//        [HttpPost]
+//        [ActionName("Delete")]
+//        [ValidateAntiForgeryToken]
+//        public ActionResult DeleteEnrollment(int id)
+//        {
+//            var service = CreateEnrollmentService();
 
-            service.DeleteEnrollment(id);
+//            service.DeleteEnrollment(id);
 
-            TempData["SaveResult"] = "The student was successfully deleted";
+//            TempData["SaveResult"] = "The student was successfully deleted";
 
-            return RedirectToAction("Index");
-        }
+//            return RedirectToAction("Index");
+//        }
 
-        private EnrollmentService CreateEnrollmentService()
-        {
-            var service = new EnrollmentService();
-            return service;
-        }
-    }
-}
+//        private EnrollmentService CreateEnrollmentService()
+//        {
+//            var service = new EnrollmentService();
+//            return service;
+//        }
+//    }
+//}
